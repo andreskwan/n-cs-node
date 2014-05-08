99d06cf2e55a70a1c49ac8faac2342716debec48
ANDRES KWAN <andreskwan@gmail.com>
2014-05-08 12:45:46 -0500

    Socket.io - working. but needs to be improved

--- public/js/main.js
+++ public/js/main.js
@@ -4,33 +4,52 @@ $(document).ready(function (){
 	window.io = io.connect(); 
 	
 	//cuando este conectado 
-	io.on('connect', function(socket){
-		console.log('hi');
-		// enviando mensajes al server
-		io.emit('Hello');
-	});
+	//Never called 
+	// io.on('connection', function(socket){
+	// 	console.log('hi');
+	// 	// enviando mensajes al server
+	// 	// generando el evento "Hello"
+	// 	io.emit('Hello');
+	// });
 
-	io.on('messages', function(data){
+	io.on('ConnectionMSG', function(data){
+	  console.log('hi');
+	  //extract the alert from the JSON
       alert(data.hello);
+      //1) name of the event to emit
+      //2) param 1
+	  //3) param 2
+      io.emit('answer', "BROADCAS: call the event: ", "answer");
     });
-	//3142155097
+	// //3142155097
+	io.on('answer', function(q,a){
+      alert(q + " " + a);
+      // debugger;
+      var nickname = prompt("what is your name?");
+      io.emit('question',nickname);
+    });
 
-	io.on('Saludo', function(data){
-		// debugger;
+    io.on('question', function(q){
+      alert(q + " " + a);
+      var nickname = prompt("what is your name?");
+      io.emit('question',nickname);
+    });
+	// io.on('Saludo', function(data){
+	// 	// debugger;
 
-		console.log(data);
-	});
+	// 	console.log(data);
+	// });
 
-	io.on('log-in', function(data){
-		// debugger;
-		 $('#users').append('<li>'+data.username+'</li>');
-	});
-	io.on('log-out', function(data){
-		// debugger;
-		$('#users li').each( function (i, item) {
-			if(item.innerText === data.username){
-				$(item).remove();
-			}
-		});
-	});
+	// io.on('log-in', function(data){
+	// 	// debugger;
+	// 	 $('#users').append('<li>'+data.username+'</li>');
+	// });
+	// io.on('log-out', function(data){
+	// 	// debugger;
+	// 	$('#users li').each( function (i, item) {
+	// 		if(item.innerText === data.username){
+	// 			$(item).remove();
+	// 		}
+	// 	});
+	// });
 });
--- socket-express.js
+++ socket-express.js
@@ -1,4 +1,4 @@
-var express = require('express');//
+var express = require('express');
 var app     = express();
 var server  = require('http').createServer(app);
 var io      = require('socket.io').listen(server);
@@ -11,29 +11,38 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
 app.get('/', function (req,res){
-  	res.render('socket');
+  	res.render('socket.html');
   	res.end();
 });
 
 io.sockets.on('connection', function(client){
 	console.log('Client connected...');
-	client.emit('messages', { hello : 'Hello Socket.io'});
+	//when client connects send this message
+	client.emit('ConnectionMSG', { hello : 'App opened'});
 
 	// listen for answers here
     client.on('answer', function(question, answer){
+    	//como se recibe este mensaje?
     	client.broadcast.emit('answer', question, answer);
+   		console.log(question + " " + answer);
     });
 
+    //I don't understand this code!!!
+    //question is an event send from the client
+    //the callback for this event
+    //validates
 	client.on('question', function(question) {
     	client.get('question_asked', function(err, asked){
+    		// debugger;
       		if(!asked){
       			client.set('question_asked', asked);
       			client.broadcast.emit('question', question);
       		}else{
+		   		console.log(asked);
       			client.set('question_asked', asked);
       		}
 		});
 	});
 });
 
-server.listen(3000);
+server.listen(3001);
